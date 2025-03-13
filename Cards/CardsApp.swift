9291b58bb135e0c124c936a4b2b9f2a04a24bb96
samlee03 <sam.lee03@myhunter.cuny.edu>
2025-03-13 18:03:32 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Jenn Lee on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cards/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cards
+//
+//  Created by Jenn Lee on 3/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
